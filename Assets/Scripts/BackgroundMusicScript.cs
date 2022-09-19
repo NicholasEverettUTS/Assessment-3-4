f86082efd8b818d49188e29a112c8eca8693ec0e
kagasquid <53798234+kagasquid@users.noreply.github.com>
2022-09-19 17:24:02 +1000

    19/09/2022 third commit, sounds complete

--- Assets/Scripts/BackgroundMusicScript.cs
+++ Assets/Scripts/BackgroundMusicScript.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 
 public class BackgroundMusicScript : MonoBehaviour
 {
+    public AudioSource thisSource;
+    public AudioClip levelstart;
+    public AudioClip backgroundNormal;
+    bool soundPlayed = false;
     // Start is called before the first frame update
     void Start()
     {
-        
+        thisSource = GetComponent<AudioSource>();
+        thisSource.PlayOneShot(levelstart, 1.0f);
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (!thisSource.isPlaying)
+        {
+            thisSource.PlayOneShot(backgroundNormal, 1.0f);
+        }
     }
 }
