a16d65c649d16a44077aa9031378b29dfac3370c
kagasquid <53798234+kagasquid@users.noreply.github.com>
2022-09-23 18:50:45 +1000

    23/09/2022 movement. PacStudent now travels in a circle. Fixinganimations.

--- Assets/Scripts/PacStudentMovementHandler.cs
+++ Assets/Scripts/PacStudentMovementHandler.cs
@@ -17,9 +17,22 @@ public class PacStudentMovementHandler : MonoBehaviour
     void Update()
     {
         Transform thisTransform = pacStudent.transform;
-        if (thisTransform.position.x == -12.0f) //&& thisTransform.position.x == 13.0f
+        if (thisTransform.position.x == -12.0f && thisTransform.position.y == 13.0f)
         {
             tweener.AddTween(pacStudent.transform, pacStudent.transform.position, new Vector3(-7.0f, 13.0f, 0.0f), 1.0f);
         }
+        if (thisTransform.position.x == -7.0f && thisTransform.position.y == 13.0f)
+        {
+            tweener.AddTween(pacStudent.transform, pacStudent.transform.position, new Vector3(-7.0f, 9.0f, 0.0f), 1.0f);
+        }
+        if (thisTransform.position.x == -7.0f && thisTransform.position.y == 9.0f)
+        {
+            tweener.AddTween(pacStudent.transform, pacStudent.transform.position, new Vector3(-12.0f, 9.0f, 0.0f), 1.0f);
+        }
+        if (thisTransform.position.x == -12.0f && thisTransform.position.y == 9.0f)
+        {
+            tweener.AddTween(pacStudent.transform, pacStudent.transform.position, new Vector3(-12.0f, 13.0f, 0.0f), 1.0f);
+        }
+
     }
 }
