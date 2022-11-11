2ec258a8d846fcd64589ca83212027676c562c59
kagasquid <53798234+kagasquid@users.noreply.github.com>
2022-11-11 17:21:28 +1100

    11/11/2022 continued work on movement

--- Assets/Scripts/PacStudentController.cs
+++ Assets/Scripts/PacStudentController.cs
@@ -8,6 +8,7 @@ public class PacStudentController : MonoBehaviour
     [SerializeField]
     private GameObject pacStudent;
     private char lastInput;
+    private char currentInput;
     int[,] levelMap =
     {
         {1,2,2,2,2,2,2,2,2,2,2,2,2,7,7,2,2,2,2,2,2,2,2,2,2,2,2,1},
@@ -22,7 +23,6 @@ public class PacStudentController : MonoBehaviour
         {1,2,2,2,2,1,5,4,3,4,4,3,0,4,4,0,3,4,4,3,4,5,1,2,2,2,2,1},
         {0,0,0,0,0,2,5,4,3,4,4,3,0,3,3,0,3,4,4,3,4,5,2,0,0,0,0,0},
         {0,0,0,0,0,2,5,4,4,0,0,0,0,0,0,0,0,0,0,4,4,5,2,0,0,0,0,0},
-        {0,0,0,0,0,2,5,4,4,0,3,4,4,0,0,4,4,3,0,4,4,5,2,0,0,0,0,0},
         {2,2,2,2,2,1,5,3,3,0,4,0,0,0,0,0,0,4,0,3,3,5,1,2,2,2,2,2},
         {0,0,0,0,0,0,5,0,0,0,4,0,0,0,0,0,0,4,0,0,0,5,0,0,0,0,0,0},
         {0,0,0,0,0,0,5,0,0,0,4,0,0,0,0,0,0,4,0,0,0,5,0,0,0,0,0,0},
@@ -54,6 +54,7 @@ public class PacStudentController : MonoBehaviour
         if (Input.GetKeyDown("d"))
         {
             lastInput = 'd';
+            
         }
 
         if (Input.GetKeyDown("s"))
@@ -71,7 +72,9 @@ public class PacStudentController : MonoBehaviour
             lastInput = 'w';
         }
 
-        switch (lastInput) { 
+        inputter(lastInput);
+
+        switch (currentInput) { 
             case 'd':
                 if(tweener.TweenExists(pacStudent.transform) == false)
                 {
@@ -96,6 +99,67 @@ public class PacStudentController : MonoBehaviour
                     tweener.AddTween(pacStudent.transform, pacStudent.transform.position, new Vector3(pacStudent.transform.position.x, pacStudent.transform.position.y + 1, 0.0f), 0.2f);
                 }
                 break;
+        }
+
+        void inputter(char c)
+        {
+            switch (c)
+            {
+                case 'd':
+                    if (pacStudent.transform.position.x + 1 > -13 && pacStudent.transform.position.x + 1 < 14)
+                    {
+                        float xCoordinate = pacStudent.transform.position.x + 1;
+                        float yCoordinate = pacStudent.transform.position.y;
+                        for (int y = 14; y > -15; y--)
+                        {
+                            for (int x = -13; x < 15; x++)
+                            {
+                                if (xCoordinate == x && yCoordinate == y)
+                                {
+                                    Debug.Log("hit");
+                                    Debug.Log(xCoordinate + 14);
+                                    //Debug.Log(yCoordinate*-1 + 14);
+                                    if (levelMap[(int)xCoordinate + 13, (int)yCoordinate * -1 + 14] == 5 ||
+                                        levelMap[(int)xCoordinate + 13, (int)yCoordinate * -1 + 14] == 6 ||
+                                        levelMap[(int)xCoordinate + 13, (int)yCoordinate * -1 + 14] == 0)
+                                    {
+                                        currentInput = 'd';
+                                        Debug.Log(currentInput);
+                                    }
+                                }
+                            }
+                        }
+                    }
+                    break;
+                case 's':
+                    if (pacStudent.transform.position.y * -1 + 1 > -14 && pacStudent.transform.position.y * -1 + 1 < 15)
+                    {
+                        float xCoordinate = pacStudent.transform.position.x;
+                        float yCoordinate = pacStudent.transform.position.y - 1;
+                        for (int y = 14; y > -15; y--)
+                        {
+                            for (int x = -13; x < 15; x++)
+                            {
+                                if (xCoordinate == x && yCoordinate == y)
+                                {
+                                    Debug.Log("hit");
+                                    Debug.Log(xCoordinate + 13);
+                                    Debug.Log(yCoordinate * -1 + 14);
+                                    if (levelMap[(int)xCoordinate + 13, (int)yCoordinate * -1 + 14] == 5 ||
+                                        levelMap[(int)xCoordinate + 13, (int)yCoordinate * -1 + 14] == 6 ||
+                                        levelMap[(int)xCoordinate + 13, (int)yCoordinate * -1 + 14] == 0)
+                                    {
+                                        currentInput = 's';
+                                        Debug.Log(currentInput);
+                                    }
+                                }
+                            }
+                        }
+                    }
+                    break;
+            }
+            
+           
         }
     }
 }
