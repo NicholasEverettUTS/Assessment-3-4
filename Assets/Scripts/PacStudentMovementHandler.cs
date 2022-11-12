12eac28420a378c1df536001b6f6befa8eaf1b08
kagasquid <53798234+kagasquid@users.noreply.github.com>
2022-11-12 21:12:10 +1100

    12/11/2022 fixing pacstudent in start screen

--- Assets/Scripts/PacStudentMovementHandler.cs
+++ Assets/Scripts/PacStudentMovementHandler.cs
@@ -22,18 +22,37 @@ public class PacStudentMovementHandler : MonoBehaviour
         if (thisTransform.position.x == -10f && thisTransform.position.y == 3f)
         {
             tweener.AddTween(pacStudent.transform, pacStudent.transform.position, new Vector3(10f, 3f, 0.0f), 3f);
+            pacStudent.GetComponent<Animator>().SetTrigger("RightWalkTrigger");
+            pacStudent.GetComponent<Animator>().ResetTrigger("DownWalkTrigger");
+            pacStudent.GetComponent<Animator>().ResetTrigger("LeftWalkTrigger");
+            pacStudent.GetComponent<Animator>().ResetTrigger("UpWalkTrigger");
         }
         if (thisTransform.position.x == 10f && thisTransform.position.y == 3f)
         {   
             tweener.AddTween(pacStudent.transform, pacStudent.transform.position, new Vector3(10f, -3f, 0.0f), 2f);
+            pacStudent.GetComponent<Animator>().enabled = true;
+            pacStudent.GetComponent<Animator>().ResetTrigger("RightWalkTrigger");
+            pacStudent.GetComponent<Animator>().SetTrigger("DownWalkTrigger");
+            pacStudent.GetComponent<Animator>().ResetTrigger("LeftWalkTrigger");
+            pacStudent.GetComponent<Animator>().ResetTrigger("UpWalkTrigger");
         }
         if (thisTransform.position.x == 10f && thisTransform.position.y == -3f)
         {
             tweener.AddTween(pacStudent.transform, pacStudent.transform.position, new Vector3(-10f, -3f, 0.0f), 3f);
+            pacStudent.GetComponent<Animator>().enabled = true;
+            pacStudent.GetComponent<Animator>().ResetTrigger("RightWalkTrigger");
+            pacStudent.GetComponent<Animator>().ResetTrigger("DownWalkTrigger");
+            pacStudent.GetComponent<Animator>().SetTrigger("LeftWalkTrigger");
+            pacStudent.GetComponent<Animator>().ResetTrigger("UpWalkTrigger");
         }
         if (thisTransform.position.x == -10f && thisTransform.position.y == -3f)
         {   
             tweener.AddTween(pacStudent.transform, pacStudent.transform.position, new Vector3(-10f, 3f, 0.0f), 2f);
+            pacStudent.GetComponent<Animator>().enabled = true;
+            pacStudent.GetComponent<Animator>().ResetTrigger("RightWalkTrigger");
+            pacStudent.GetComponent<Animator>().ResetTrigger("DownWalkTrigger");
+            pacStudent.GetComponent<Animator>().ResetTrigger("LeftWalkTrigger");
+            pacStudent.GetComponent<Animator>().SetTrigger("UpWalkTrigger");
         }
 
     }
