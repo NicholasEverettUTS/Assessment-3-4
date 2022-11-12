1273d99eac254e9fa74ca87b0205acafac6e9392
kagasquid <53798234+kagasquid@users.noreply.github.com>
2022-11-12 16:08:17 +1100

    12/11/2022, working on audio

--- Assets/Scripts/PacStudentController.cs
+++ Assets/Scripts/PacStudentController.cs
@@ -7,10 +7,19 @@ public class PacStudentController : MonoBehaviour
     private Tweener tweener;
     [SerializeField]
     private GameObject pacStudent;
+    [SerializeField]
+    private AudioClip clip1;
+    [SerializeField]
+    private AudioClip clip2;
+    [SerializeField]
+    private AudioClip clip3;
+    private AudioSource sound;
     private char lastInput;
     private char currentInput;
     bool doMove = false;
     Animator animator;
+    private ParticleSystem particles;
+    private GameObject particleObject;
     int[,] levelMap =
     {
         {1,2,2,2,2,2,2,2,2,2,2,2,2,7,7,2,2,2,2,2,2,2,2,2,2,2,2,1},
@@ -50,7 +59,11 @@ public class PacStudentController : MonoBehaviour
     {
         tweener = GetComponent<Tweener>();
         pacStudent.GetComponent<Animator>().enabled = false;
-
+        particleObject = pacStudent.transform.GetChild(0).gameObject;
+        particles = pacStudent.transform.GetChild(0).gameObject.GetComponent<ParticleSystem>();
+        particles.Stop(true);
+        sound = pacStudent.GetComponent<AudioSource>();
+        //sound.enabled = false;
     }
 
     // Update is called once per frame
@@ -58,8 +71,7 @@ public class PacStudentController : MonoBehaviour
     {
         if (Input.GetKeyDown("d"))
         {
-            lastInput = 'd';
-            
+            lastInput = 'd';  
         }
 
         if (Input.GetKeyDown("s"))
@@ -79,38 +91,7 @@ public class PacStudentController : MonoBehaviour
 
         inputter(lastInput);
         obstacleCheck(currentInput);
-        //Debug.Log(currentInput);
-
-        switch (currentInput) { 
-            case 'd':
-                if(tweener.TweenExists(pacStudent.transform) == false)
-                {
-                    if(doMove == true)
-                        tweener.AddTween(pacStudent.transform, pacStudent.transform.position, new Vector3(pacStudent.transform.position.x + 1, pacStudent.transform.position.y, 0.0f), 0.2f);
-                }
-                break;
-            case 's':
-                if (tweener.TweenExists(pacStudent.transform) == false)
-                {
-                    if (doMove == true)
-                        tweener.AddTween(pacStudent.transform, pacStudent.transform.position, new Vector3(pacStudent.transform.position.x, pacStudent.transform.position.y - 1, 0.0f), 0.2f);
-                }
-                break;
-            case 'a':
-                if (tweener.TweenExists(pacStudent.transform) == false)
-                {
-                    if (doMove == true)
-                        tweener.AddTween(pacStudent.transform, pacStudent.transform.position, new Vector3(pacStudent.transform.position.x - 1, pacStudent.transform.position.y, 0.0f), 0.2f);
-                }
-                break;
-            case 'w':
-                if (tweener.TweenExists(pacStudent.transform) == false)
-                {
-                    if (doMove == true)
-                        tweener.AddTween(pacStudent.transform, pacStudent.transform.position, new Vector3(pacStudent.transform.position.x, pacStudent.transform.position.y + 1, 0.0f), 0.2f);
-                }
-                break;
-        }
+        mover(currentInput);
 
         void inputter(char c)
         {
@@ -149,7 +130,6 @@ public class PacStudentController : MonoBehaviour
                             {
                                 if (xCoordinate == x && yCoordinate == y)
                                 {
-                                    //Debug.Log(levelMap[((int)y * -1 + 14), ((int)x + 13)]);
                                     if (levelMap[(int)yCoordinate * -1 + 14, (int)xCoordinate + 13] == 5 ||
                                         levelMap[(int)yCoordinate * -1 + 14, (int)xCoordinate + 13] == 6 ||
                                         levelMap[(int)yCoordinate * -1 + 14, (int)xCoordinate + 13] == 0)
@@ -163,7 +143,7 @@ public class PacStudentController : MonoBehaviour
                     }
                     break;
                 case 'a':
-                    if (pacStudent.transform.position.x + 1 > -13 && pacStudent.transform.position.x + 1 < 14)
+                    if (pacStudent.transform.position.x + 1 > -13 && pacStudent.transform.position.x + 1 < 15)
                     {
                         float xCoordinate = pacStudent.transform.position.x - 1;
                         float yCoordinate = pacStudent.transform.position.y;
@@ -185,9 +165,8 @@ public class PacStudentController : MonoBehaviour
                     }
                     break;
                 case 'w':
-                    if (pacStudent.transform.position.y * - 1 - 1 > -14 && pacStudent.transform.position.y * - 1 - 1 < 15)
+                    if (pacStudent.transform.position.y * - 1 - 1 > -15 && pacStudent.transform.position.y * - 1 - 1 < 15)
                     {
-                        Debug.Log("hit");
                         float xCoordinate = pacStudent.transform.position.x;
                         float yCoordinate = pacStudent.transform.position.y + 1;
                         for (int y = 14; y > -15; y--)
@@ -196,7 +175,6 @@ public class PacStudentController : MonoBehaviour
                             {
                                 if (xCoordinate == x && yCoordinate == y)
                                 {
-                                    Debug.Log(levelMap[((int)y * -1 + 14), ((int)x + 13)]);
                                     if (levelMap[(int)yCoordinate * -1 + 14, (int)xCoordinate + 13] == 5 ||
                                         levelMap[(int)yCoordinate * -1 + 14, (int)xCoordinate + 13] == 6 ||
                                         levelMap[(int)yCoordinate * -1 + 14, (int)xCoordinate + 13] == 0)
@@ -236,11 +214,16 @@ public class PacStudentController : MonoBehaviour
                                         pacStudent.GetComponent<Animator>().ResetTrigger("DownWalkTrigger");
                                         pacStudent.GetComponent<Animator>().ResetTrigger("LeftWalkTrigger");
                                         pacStudent.GetComponent<Animator>().ResetTrigger("UpWalkTrigger");
+                                        particleObject.SetActive(true);
+                                        audioSwitch(levelMap[(int)yCoordinate * -1 + 14, (int)xCoordinate + 13]);
+                                        particles.Play();
                                     }
                                     else
                                     {
                                         doMove = false;
+                                        audioSwitch(levelMap[(int)yCoordinate * -1 + 14, (int)xCoordinate + 13]);
                                         pacStudent.GetComponent<Animator>().enabled = false;
+                                        particles.Stop(true);
                                     }
                                 }
                             }
@@ -268,11 +251,16 @@ public class PacStudentController : MonoBehaviour
                                         pacStudent.GetComponent<Animator>().SetTrigger("DownWalkTrigger");
                                         pacStudent.GetComponent<Animator>().ResetTrigger("LeftWalkTrigger");
                                         pacStudent.GetComponent<Animator>().ResetTrigger("UpWalkTrigger");
+                                        particleObject.SetActive(true);
+                                        audioSwitch(levelMap[(int)yCoordinate * -1 + 14, (int)xCoordinate + 13]);
+                                        particles.Play();
                                     }
                                     else
                                     {
                                         doMove = false;
                                         pacStudent.GetComponent<Animator>().enabled = false;
+                                        audioSwitch(levelMap[(int)yCoordinate * -1 + 14, (int)xCoordinate + 13]);
+                                        particles.Stop(true);
                                     }
                                 }
                             }
@@ -300,11 +288,16 @@ public class PacStudentController : MonoBehaviour
                                         pacStudent.GetComponent<Animator>().ResetTrigger("DownWalkTrigger");
                                         pacStudent.GetComponent<Animator>().SetTrigger("LeftWalkTrigger");
                                         pacStudent.GetComponent<Animator>().ResetTrigger("UpWalkTrigger");
+                                        particleObject.SetActive(true);
+                                        audioSwitch(levelMap[(int)yCoordinate * -1 + 14, (int)xCoordinate + 13]);
+                                        particles.Play();
                                     }
                                     else
                                     {
                                         doMove = false;
                                         pacStudent.GetComponent<Animator>().enabled = false;
+                                        audioSwitch(levelMap[(int)yCoordinate * -1 + 14, (int)xCoordinate + 13]);
+                                        particles.Stop(true);
                                     }
                                 }
                             }
@@ -312,7 +305,7 @@ public class PacStudentController : MonoBehaviour
                     }
                     break;
                 case 'w':
-                    if (pacStudent.transform.position.y * - 1 - 1 > -14 && pacStudent.transform.position.y * - 1 - 1 < 15)
+                    if (pacStudent.transform.position.y * - 1 - 1 > -15 && pacStudent.transform.position.y * - 1 - 1 < 15)
                     {
                         float xCoordinate = pacStudent.transform.position.x;
                         float yCoordinate = pacStudent.transform.position.y + 1;
@@ -322,7 +315,6 @@ public class PacStudentController : MonoBehaviour
                             {
                                 if (xCoordinate == x && yCoordinate == y)
                                 {
-                                    //Debug.Log(levelMap[((int)y * -1 + 14), ((int)x + 13)]);
 
                                     if (levelMap[(int)yCoordinate * -1 + 14, (int)xCoordinate + 13] == 5 ||
                                         levelMap[(int)yCoordinate * -1 + 14, (int)xCoordinate + 13] == 6 ||
@@ -334,17 +326,75 @@ public class PacStudentController : MonoBehaviour
                                         pacStudent.GetComponent<Animator>().ResetTrigger("DownWalkTrigger");
                                         pacStudent.GetComponent<Animator>().ResetTrigger("LeftWalkTrigger");
                                         pacStudent.GetComponent<Animator>().SetTrigger("UpWalkTrigger");
+                                        particleObject.SetActive(true);
+                                        audioSwitch(levelMap[(int)yCoordinate * -1 + 14, (int)xCoordinate + 13]);
+                                        particles.Play();
                                     }
                                     else
                                     {
                                         doMove = false;
                                         pacStudent.GetComponent<Animator>().enabled = false;
+                                        audioSwitch(levelMap[(int)yCoordinate * -1 + 14, (int)xCoordinate + 13]);
+                                        particles.Stop(true);
                                     }
                                 }
                             }
                         }
                     }
                     break;
+            }
+        }
+        void mover(char c)
+        {
+            switch (c)
+            {
+                case 'd':
+                    if (tweener.TweenExists(pacStudent.transform) == false)
+                    {
+                        if (doMove == true)
+                            tweener.AddTween(pacStudent.transform, pacStudent.transform.position, new Vector3(pacStudent.transform.position.x + 1, pacStudent.transform.position.y, 0.0f), 0.2f);
+                    }
+                    break;
+                case 's':
+                    if (tweener.TweenExists(pacStudent.transform) == false)
+                    {
+                        if (doMove == true)
+                            tweener.AddTween(pacStudent.transform, pacStudent.transform.position, new Vector3(pacStudent.transform.position.x, pacStudent.transform.position.y - 1, 0.0f), 0.2f);
+                    }
+                    break;
+                case 'a':
+                    if (tweener.TweenExists(pacStudent.transform) == false)
+                    {
+                        if (doMove == true)
+                            tweener.AddTween(pacStudent.transform, pacStudent.transform.position, new Vector3(pacStudent.transform.position.x - 1, pacStudent.transform.position.y, 0.0f), 0.2f);
+                    }
+                    break;
+                case 'w':
+                    if (tweener.TweenExists(pacStudent.transform) == false)
+                    {
+                        if (doMove == true)
+                            tweener.AddTween(pacStudent.transform, pacStudent.transform.position, new Vector3(pacStudent.transform.position.x, pacStudent.transform.position.y + 1, 0.0f), 0.2f);
+                    }
+                    break;
+            }
+        }
+        void audioSwitch(int i)
+        {
+            Debug.Log("hit");
+            if (i == 5 || i == 6)
+            {
+                sound.enabled = true;
+                sound.clip = clip2;
+            }
+            else if(i == 0)
+            {
+                sound.enabled = true;
+                sound.clip = clip2;
+            }
+            else
+            {
+                sound.clip = clip2;
+                sound.enabled = false;
             }
         }
     }
